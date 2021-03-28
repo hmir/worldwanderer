3a189fed09ff5c0847bbeef7d08b489d1c994778
Hamza Mir <hmir@Hamzas-MacBook-Pro.local>
2021-03-28 06:25:05 -0400

    Add sustainability challenge

--- code.js
+++ code.js
@@ -2,6 +2,38 @@ const DISTANCE_THRESHOLD = 6_000_000;
 
 var panorama;
 var latLng;
+let globalMap;
+
+let curChallenge;
+const sustainabilityChallengeLocations = [
+  {
+    name: 'The Great Barrier Reef',
+    question: "The ___ is the world's largest coral reef system composed of over 2,900 individual reefs and 900 islands stretching for over 2,300 kilometres. This reef structure is composed of and built by billions of tiny organisms, known as coral polyps. According to a study published in October 2012 by the Proceedings of the National Academy of Sciences, the reef has lost more than half its coral cover since 1985, a finding reaffirmed by a 2020 study which found over half of the reef's coral cover to have been lost between 1995 and 2017, with the effects of a widespread 2020 bleaching event not yet quantified",
+    location: {lat: -23.3071842, lng: 151.9144542},
+  },
+  // {
+  //   name: "Ganges River",
+  //   question: "The ___ is a trans-boundary river which flows through ___ and ___. It is the third largest river on Earth by discharge. The ___ is threatened by severe pollution. This poses a danger not only to humans but also to animals; the ___ is home to approximately 140 species of fish and 90 species of amphibians. The river also contains reptiles and mammals, including critically endangered species such as the gharial.",
+  //   location: {lat: 27.4036401, lng:83.0319706},
+  // },
+  {
+    name: "The Hudson River",
+    question: "The ___ is a 315-mile (507 km) river that flows from north to south primarily through eastern ___. The ___'s sediments contain a significant array of pollutants, accumulated over decades from industrial waste discharges, sewage treatment plants, and urban runoff. The most discussed pollution of the ___ is General Electric's contamination of the river with polychlorinated biphenyls (PCBs) between 1947 and 1977. This pollution caused a range of harmful effects to wildlife and people who ate fish from the river or drank the water.",
+    location: {lat: 40.8358816, lng: -74.01432}
+  },
+  {
+    name: "Beijing",
+    question: "This city, with a population of over 21 million residents, is  one of the world's leading centers for culture, diplomacy and politics, business and economics, education, language, and science and technology. ___ has a long history of environmental problems. Because of the combined factors of urbanization and pollution caused by burning of fossil fuel, ___ is often affected by serious environmental problems, which lead to health issues of many inhabitants. In 2013 heavy smog struck ___ and most parts of ___, impacting a total of 600 million people. After this 'pollution shock' the government of ____ announced measures to reduce air pollution.",
+    location: {lat: 39.9682234, lng: 116.4770841}
+    
+  }, 
+  {
+    name: "Central Park, NY",
+    question: "____ 18,000 trees and 843 acres of trails, lawns, and gardens offer some much-needed reprieve when it's area turns into what's called an 'urban heat island.' More concrete and less tree cover exacerbates this difference in temperature, which can vary by 16 degrees depending on location. This is because hard, impervious surfaces (like paved roadways and empty rooftops) absorb, magnify, and slowly release heat throughout the course of a day, creating a warming cycle on city streets.",
+    location: {lat: 40.7695199, lng: -73.9724093}
+  }
+
+]
 
 function randomLatLng() {
   return {
@@ -48,11 +80,34 @@ function markResultsMap(guessLatLng, targetLatLng) {
 }
 
 let success = false;
-async function setRandomStreetView() {
-  console.log("making req");
+async function setRandomStreetView(outdoor) {
   let coords = randomLatLng();
+  
+  // sustainability challenge
+  if (!outdoor) {
+    let cookie = document.cookie;
+    console.log(document.cookie);
+    if (cookie.length == 0) {
+      document.cookie = '0';
+      curChallenge = 0;
+    } else {
+      curChallenge = parseInt(cookie) + 1;      
+    }
+    if (curChallenge == sustainabilityChallengeLocations.length) {
+      document.cookie = (curChallenge + 1).toString();
+      location.href = location.origin + '/success.html';
+    } else if (curChallenge > sustainabilityChallengeLocations.length) {
+      document.cookie = '0';
+      curChallenge = 0;
+    }
+    console.log('chalnum', curChallenge);
+    coords = sustainabilityChallengeLocations[curChallenge].location;
+    $('#info').css('display', 'inline');
+    $('#info').text(sustainabilityChallengeLocations[curChallenge].question);
+  }
+
   console.log(coords);
-  let req = { location: coords, preference: 'nearest', radius: 400000, source: 'outdoor' };
+  let req = {location: coords, preference: 'nearest', radius: 400000, source: outdoor ? 'outdoor' : 'default'};
   let sv = new google.maps.StreetViewService();
 
   try {
@@ -74,6 +129,11 @@ async function setRandomStreetView() {
   }
 }
 
+function isSustainabilityChallenge() {
+  let searchParams = new URLSearchParams(window.location.search);
+  return searchParams.has('sustainability') && searchParams.get('sustainability') == 'true';
+}
+
 async function initialize() {
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 0, lng: 0 },
@@ -85,7 +145,13 @@ async function initialize() {
     placeMarkerAndPanTo(e.latLng, map);
   });
 
-  while (await setRandomStreetView() == false);
+  if(isSustainabilityChallenge()) {
+    $("#sust-challenge").css('display', 'none');
+  }
+
+  globalMap = map;
+
+  while (await setRandomStreetView(!isSustainabilityChallenge()) == false);
 }
 
 var marker;
@@ -115,6 +181,12 @@ function makeGuess() {
   let ratio = progress / DISTANCE_THRESHOLD;
   $("#progress-bar-container").css('display', "inline");
   $("#progress-bar").attr('aria-valuenow', progress).css('width', Math.max(1, ratio * 100) + "%");
-  $("#progress-text").text('You were ' + Math.ceil(dist / 1000) + ' km away... Score: ' + Math.ceil(ratio * 100) + '%');
+  if (isSustainabilityChallenge()) {
+    document.cookie = curChallenge.toString();
+    let name = sustainabilityChallengeLocations[curChallenge].name;
+    $("#progress-text").text('The location was ' + name + '. You were ' + Math.ceil(dist / 1000) + ' km away.');
+  } else {
+    $("#progress-text").text('You were ' + Math.ceil(dist / 1000) + ' km away... Score: ' + Math.ceil(ratio * 100) + '%');
+  }
   $("#play-again").text('Hit here to play again!');
 }
