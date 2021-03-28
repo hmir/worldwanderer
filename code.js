c6a51e3662517f38b200a19e2d9934e9af1aeaec
Hamza Mir <hmir@Hamzas-MacBook-Pro.local>
2021-03-28 02:29:24 -0400

    Increase min bar size

--- code.js
+++ code.js
@@ -112,7 +112,7 @@ function makeGuess() {
   console.log('rat', progress/DISTANCE_THRESHOLD);
   let ratio = progress/DISTANCE_THRESHOLD;
   $("#progress-bar-container").css('display', "inline");
-  $("#progress-bar").attr('aria-valuenow', progress).css('width', ratio * 100 + "%");
+  $("#progress-bar").attr('aria-valuenow', progress).css('width', Math.max(1, ratio * 100) + "%");
   $("#progress-text").text('You were ' + Math.ceil(dist/1000) + ' km away... Score: ' + Math.ceil(ratio * 100) + '%');
 }
 
